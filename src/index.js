f388b667665eead2d93e5fa44136f83acfc61716
jamesandrews23 <james.andrews23@gmail.com>
2020-07-16 10:07:08 -0500

    Template literal to display buttons and content within body of popover
    Makes defaults accessible outside plugin using popit namespace
    
    Triggers custom click events for confirm and cancel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 +function($){
-    $('#test1').popit();
+    $('#test1').popit().on("clicked.pop.confirm clicked.pop.cancel", function(event, btnEl){
+        console.log("button " + btnEl + " clicked");
+    });
+
 }(jQuery);
--- src/jquery-plugin-popit.js
+++ src/jquery-plugin-popit.js
@@ -1,22 +1,44 @@
 +function($){
-    $.fn.popit = function(options){
+    const getTemplate = function(btnConfirmTitle, btnCancelTitle, content){
+        return `<div style="text-align: center">${content}</div>
+                <div style="text-align:center">
+                <button class="btn btn-primary btn-sm" id="popitBtnConfirm" type="button">${btnConfirmTitle}</button>
+                <button class="btn btn-default btn-sm" id="popitBtnCancel" type="button">${btnCancelTitle}</button>
+                </div>`;
+    };
 
-        var settings = $.extend({
-            title: "title", //popover's title
-            content: "some content from settings", //popover's body
-            placement: "top", //where popover appears
-            template: null //if template is defined it will be used in place of default template
-        }, options); //element data- values will override options or defaults
-
+    $.fn.popit = function(userOptions){
         return this.each(function(){
-            var $el = $(this);
+            let $el = $(this);
 
-            $el.popover({
-                placement: $el.data("placement") || settings.placement,
-                title: $el.data("title") || settings.title,
-                content: $el.data("content") || settings.content,
-                trigger: "click"
+            //override data-attributes take precedence over passed options, which take precedence over defaults
+            let options = $.extend({}, $.fn.popit.defaults, userOptions, $el.data());
+
+            //set the content to the template with the confirm and cancel buttons
+            options.content = getTemplate(options.btnConfirmTitle, options.btnCancelTitle, options.content);
+
+            $el.popover(options).on("inserted.bs.popover", function(){
+                $('#popitBtnConfirm').click(function(){
+                    $el.trigger("clicked.pop.confirm", [$el]);
+                    $el.popover("hide");
+                });
+
+                $('#popitBtnCancel').click(function(){
+                    $el.trigger("clicked.pop.cancel", [$el]);
+                    $el.popover("hide");
+                });
+
             });
         });
+    };
+
+    $.fn.popit.defaults = {
+        title: "title", //popover's title
+        content: "some content from settings", //popover's body
+        placement: "top", //where popover appears
+        btnConfirmTitle: "Confirm",
+        btnCancelTitle: "Cancel",
+        html:true,
+        sanitize: false
     }
 }(jQuery);
